1a3df4d1a62ed53ac28dab8321ec2b7b99325258
dsKU <54886969+dsKU@users.noreply.github.com>
2022-05-26 10:57:54 +0900

    웹채팅 기능만 구현

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
     <div id="app" style="overflow-x: hidden">
         <side-bar></side-bar>
         <router-view/>
+        <div class="chat-div">
+            <textarea v-model="chatMesseges" style="height:300px;"></textarea>
+            <textarea class="form-control" placeholder="Leave a comment here" v-model="chatMessege"></textarea>
+            <button @click="send" @keydown.enter="send">전송</button>
+        </div>
     </div>
 </template>
 
@@ -13,11 +18,41 @@ export default {
     components: { SideBar },
     data() {
         return {
-
+            webSocket : null,
+            chatMesseges :[],
+            chatMessege : "",
+            sendMessege : {
+                "type" : "ENTER",
+                "sender" : "zzzz",
+                "message" : "ㅎㅇ"
+                }
         }
     },
+    mounted(){
+        this.webSocket = new WebSocket("ws://localhost:8080/ws/chat");
+        this.webSocket.onopen = this.onOpen;
+        this.webSocket.onclose = this.onClose;
+        this.webSocket.onmessage = this.onMessage;
+    },
     methods: {
+        onOpen : function(){
+            
+            this.webSocket.send(JSON.stringify(this.sendMessege));
+            console.log("웹소캣 연결");
+            console.log(this.webSocket);
+        },
+        onClose : function(){
 
+        },
+        onMessage : function(e){
+            console.log(e)
+            this.chatMesseges.push(e.data);
+        },
+        send : function(){
+            this.sendMessege.message = this.chatMessege;
+            this.webSocket.send(JSON.stringify(this.sendMessege));
+            console.log("전송");
+        }
     }
 };
 </script>
@@ -32,5 +67,15 @@ export default {
 .change-small {
     padding-left: 102px !important;
     transition-duration: 0.5s;
+}
+
+.chat-div {
+    position : absolute;
+    top : 10%;
+    left : 20%;
+    width:  200px;
+    height: 400px;
+    background-color: antiquewhite;
+    z-index: 10;
 }
 </style>
